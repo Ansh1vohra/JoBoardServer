2fe8b61c7ec27aeffd870a7ed35b8803ca7a43e8
Ansh1vohra <anshvohra1@gmail.com>
2024-10-09 02:11:37 +0530

    NewBackend

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const { connectDB } = require('./config/db');
 const userRoutes = require('./routes/user');
 const jobRoutes = require('./routes/jobs');
+const applicationRoutes = require('./routes/application');
+const companyRoutes = require('./routes/company');
 
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 app.use('/api/jobs', jobRoutes);
+app.use('/api/application', applicationRoutes);
+app.use('/api/company', companyRoutes);
 
 const PORT = process.env.PORT || 5000;
 
